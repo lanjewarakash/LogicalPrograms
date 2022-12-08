4507952896a37a87a116e3fad14810226c5af6db
lanjewarakash <lanjewarakash72@gmail.com>
2022-12-08 10:52:04 +0530

    Added : Prime Number Programs

---
+++ src/PrimeNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter A value");
+        int num = sc.nextInt();
+        checkPrime(num);
+
+    }
+
+    public static void checkPrime(int a) {
+        int b = 0;
+        for (int i = 2; i <= a / 2; i++) {
+            if (a % i == 0) {
+                b++;
+            }
+        }
+        if (b == 0) {
+            System.out.println("Number Is Prime");
+
+        } else {
+            System.out.println("NUmber is Not Prime");
+        }
+    }
+}
